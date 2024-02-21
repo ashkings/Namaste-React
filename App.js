c21bef0afc7aa69365b041033b603bc4a50a8d06
Ashu Singla <ashusingla884@gmail.com>
2024-02-21 15:45:23 +0530

    feat:episode-01

---
+++ App.js
@@ -0,0 +1,22 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am a H1 tag"),
+    React.createElement("h2", {}, "I am a H2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am a H1 tag"),
+    React.createElement("h2", {}, "I am a H2 tag"),
+  ]),
+]);
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello world! from React!"
+);
+
+console.log(heading); // object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
