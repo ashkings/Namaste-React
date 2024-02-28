fed8dd11de13c0d4fdf414e76f8d48c0af8b4070
Ashu Singla <ashusingla884@gmail.com>
2024-02-28 16:02:44 +0530

    feat: add dummy components

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <dic>
+      <h1>About</h1>
+      <h2>This is Namaste React Web series</h2>
+    </dic>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <dic>
+      <h1>Contact</h1>
+      <h2>This is Namaste React Web series</h2>
+    </dic>
+  );
+};
+
+export default Contact;
