f8420274a7e34d1489b1cdc5c142af5727a09677
Ashu Singla <ashusingla884@gmail.com>
2024-02-28 16:04:04 +0530

    feat: add API url's

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,11 @@
 export const LOGO_URL =
-  "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
+  "https://wixmp-fe53c9ff592a4da924211f23.wixmp.com/users/a55913ef-da4d-4e37-9a8d-6bce0a4cf363/design-previews/6c43db20-1ecf-4b79-9a09-8fe071b43faa/1709019770270-thumbnail.jpeg";
 
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const SWIGGY_API_URL =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9583852&lng=77.643915&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
+export const MENU_API_URL =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9583852&lng=77.643915&restaurantId=";
