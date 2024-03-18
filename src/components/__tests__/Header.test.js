137e40cde7a31b95a5ab98e3afac8be56e39e1c8
Ashu Singla <ashusingla884@gmail.com>
2024-03-18 19:18:20 +0530

    feat: unit test for Header component

---
+++ src/components/__tests__/Header.test.js
@@ -0,0 +1,52 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
+import Header from "../Header";
+import appStore from "../../utils/redux/appStore";
+
+it("Should load header component with login button", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const loginButton = screen.getByRole("button", { name: "Login" });
+
+  expect(loginButton).toBeInTheDocument();
+});
+
+it("Should render Header component with 0 cart items", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const cartItems = screen.getByText("Cart - (0 items)");
+
+  expect(cartItems).toBeInTheDocument();
+});
+
+it("Should change Login Button to Logount on click", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const loginButton = screen.getByRole("button", { name: "Login" });
+
+  fireEvent.click(loginButton);
+
+  const logoututton = screen.getByRole("button", { name: "Logout" });
+
+  expect(logoututton).toBeInTheDocument();
+});
